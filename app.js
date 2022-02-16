57887ec21522615a52f986de478e21d113d4cd6d
MunnY <mahmudaaktermunny4@gmail.com>
2022-02-16 23:51:42 +0600

    first commit

--- app.js
+++ app.js
@@ -8,3 +8,34 @@ function navToggle()
 	//console.log('hi');
 }
 navToggler.addEventListener("click",navToggle);
+
+
+//theme
+
+function themeLightDark(){
+const switcherBtn = document.querySelector(".js-switcher-btn");
+const icon = switcherBtn.querySelector("i");
+
+function changeIcon(){
+	if(document.body.classList.contains("dark")){
+		icon.classList.remove("fa-moon-o");
+		icon.classList.add("fa-sun-o");
+	}
+	else{
+		icon.classList.remove("fa-sun-o");
+		icon.classList.add("fa-moon-o");
+	}
+}
+switcherBtn.addEventListener("click",()=>{
+	document.body.classList.toggle("dark");
+	changeIcon();
+	if(document.body.classList.contains("dark")){
+		IconStorage.setItem("theme","dark");
+	}
+	else{
+		IconStorage.setItem("theme","light");
+	}
+});
+changeIcon();
+}
+themeLightDark();
